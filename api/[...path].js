6071d9677d98236ae70d43f97a96fd7bf6d0ab08
suhas <suhasbs229@gmail.com>
2025-08-18 23:36:04 +0530

    Add detailed error handling to login and remove deprecated catchall file

--- api/[...path].js
+++
@@ -1,4 +0,0 @@
-// This file is being replaced by specific endpoint files
-export default async function handler(req, res) {
-  res.status(404).json({ success: false, message: 'This endpoint is deprecated' });
-}
